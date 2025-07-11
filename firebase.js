a23d4191760151b1921cbd3bcbcd5e2cb6d3ee78
Pecorine <harahellima@gmail.com>
2025-07-11 19:20:00 -0300

    asd

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC0sWmc2-VlUKQODhFYgoHm5X5Vgu-VAlw",
+  authDomain: "generetorwallpaper.firebaseapp.com",
+  projectId: "generetorwallpaper",
+  storageBucket: "generetorwallpaper.firebasestorage.app",
+  messagingSenderId: "365929986387",
+  appId: "1:365929986387:web:07f4f9b9365602a20949a6"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
